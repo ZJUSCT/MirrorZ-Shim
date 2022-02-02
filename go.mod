416279c21608a4573b0c198a690d9936d82a2ed5
NCJ <me@ncj.wiki>
2022-02-02 17:05:01 +0800

    add cache

--- go.mod
+++ go.mod
@@ -4,12 +4,17 @@ go 1.17
 
 require (
 	github.com/labstack/echo/v4 v4.6.3
+	github.com/dgraph-io/ristretto v0.1.0
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/ZJUSCT/MirrorZ-Shim/convertor"
 	"github.com/ZJUSCT/MirrorZ-Shim/models"
+	"github.com/dgraph-io/ristretto"
 	"github.com/labstack/echo/v4"
 	"io"
 	"net/http"
+	"time"
 )
 
 func main() {
+	cache, err := ristretto.NewCache(&ristretto.Config{
+		NumCounters: 100,
+		MaxCost:     1000,
+		BufferItems: 64,
+	})
+	if err != nil {
+		panic(err)
+	}
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		e.Logger.Info("Incoming ping request")
@@ -18,22 +30,32 @@ func main() {
 	e.GET("/mirrorz.json", func(c echo.Context) error {
 		e.Logger.Info("Incoming mirrorz.json request")
 
-		resp, err := http.Get("https://mirrors.zju.edu.cn/api/mirrors")
-		if err != nil {
-			return err
-		}
-		defer func(Body io.ReadCloser) {
-			_ = Body.Close()
-		}(resp.Body)
+		var data *models.MirrorZ
+		val, found := cache.Get("mirrorz")
+		if !found {
+			fmt.Println("no cache")
+			resp, err := http.Get("https://mirrors.zju.edu.cn/api/mirrors")
+			if err != nil {
+				return err
+			}
+			defer func(Body io.ReadCloser) {
+				_ = Body.Close()
+			}(resp.Body)
 
-		body, err := io.ReadAll(resp.Body)
-		mirrorData := models.Mirror{}
-		err = json.Unmarshal(body, &mirrorData)
-		if err != nil {
-			return err
-		}
+			body, err := io.ReadAll(resp.Body)
+			mirrorData := models.Mirror{}
+			err = json.Unmarshal(body, &mirrorData)
+			if err != nil {
+				return err
+			}
 
-		data := convertor.Convert(mirrorData)
+			data = convertor.Convert(mirrorData)
+
+			cache.SetWithTTL("mirrorz", data, 1, time.Minute*5)
+		} else {
+			fmt.Println("has cache")
+			data = val.(*models.MirrorZ)
+		}
 
 		if err := c.Bind(data); err != nil {
 			return err
